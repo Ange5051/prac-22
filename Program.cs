4acfe70e3f9e569a0636fd17554c9d2b407b1df8
Ange5051 <147865401+Ange5051@users.noreply.github.com>
2025-02-13 11:11:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { }
+        class Calculate
+        {
+            public double field_1;
+            public double field_2;
+            public Calculate() { }
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="field_1"></param>
+            public Calculate(double field_1)
+            {
+                this.field_1 = field_1;
+            }
+            public Calculate(double field1 doublefield2) : this(double field 1);
+                p
+               
+        }
+
+    }
+    }
